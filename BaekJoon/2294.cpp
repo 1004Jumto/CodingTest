8395240cc49870b33fbdf5bc2c5d2021b0adc50a
YEJIN <leeyjin0926@gmail.com>
2023-08-12 23:45:53 +0900

    2023-08-12-Sol BOJ

---
+++ BaekJoon/2294.cpp
@@ -0,0 +1,30 @@
+/*
+* [백준 2294] 동전2
+* n가지 종류의 동전이 있다. 이 동전들을 적당히 사용해서, 그 가치의 합이 k원이 되도록 하고 싶다. 
+* 그러면서 동전의 개수가 최소가 되도록 하려고 한다. 각각의 동전은 몇 개라도 사용할 수 있다.
+
+* 사용한 동전의 구성이 같은데, 순서만 다른 것은 같은 경우이다.
+
+* [알고리즘]
+* 다이나믹 프로그래밍
+* 동적계획법으로 풀기 위해서는 점화식과 배열에 저장한다는 것이 포인트이다!
+
+* 
+
+*/
+
+#include<iostream>
+#include<algorithm>
+#include<vector>	
+
+#define MAX 1500001
+
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	return 0;
+}
