d6180df427c656ab936869f481a0a5e1a839f653
YEJIN <leeyjin0926@gmail.com>
2023-08-16 00:23:57 +0900

    2023-08-15-Modify 9251

--- BaekJoon/9251.cpp
+++ BaekJoon/9251.cpp
@@ -35,7 +35,7 @@ int main(void) {
 	for (int i = 0; i <= B.length(); i++) dp[i][0] = 0;
 	for (int i = 1; i <= B.length(); i++) {
 		for (int j = 1; j <= A.length(); j++)
-			if (B[i] == A[j]) dp[i][j] = dp[i - 1][j - 1] + 1;
+			if (B[i-1] == A[j-1]) dp[i][j] = dp[i - 1][j - 1] + 1;
 			else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
 	}
 
@@ -43,40 +43,4 @@ int main(void) {
 
 	return 0;
 }
-//#include <iostream>
-//#include <string>
-//using namespace std;
-//int dp[1002][1002];
-//int mx(int a, int b) {
-//    return a > b ? a : b; // 삼항 연산자
-//}
-//int main() {
-//    string s1;
-//    string s2;
-//    cin >> s1 >> s2;
-//    dp[0][0] = 0;
-//    for (int i = 0; i <= s1.length(); i++) {
-//        dp[i][0] = 0;
-//    } for (int i = 0; i <= s2.length(); i++) {
-//        dp[0][i] = 0;
-//    }
-//    for (int i = 1; i <= s1.length(); i++)
-//    {
-//        for (int j = 1; j <= s2.length(); j++)
-//        {
-//            if (s1[i - 1] == s2[j - 1])
-//            {
-//                dp[i][j] = dp[i - 1][j - 1] + 1;
-//                // 문자열이 같아 lcs하나 늘려줌
-//            }
-//            else
-//            {
-//                dp[i][j] = mx(dp[i][j - 1], dp[i - 1][j]);
-//                //둘 중 더 큰 거 선택
-//            }
-//        }
-//    }
-//    cout << dp[s1.length()][s2.length()];
-//    return 0;
-//
-//}
+
